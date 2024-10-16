5d190588c435e1e12f1c4fb1d56a52c73e3b215c
Shade78 <shademoon322@gmail.com>
2024-10-16 04:38:55 +0300

    added email editor functionality

--- src/components/email-editor/EmailEditor.jsx
+++ src/components/email-editor/EmailEditor.jsx
@@ -1,30 +1,62 @@
+import { useState, useRef } from "react";
+import parse from "html-react-parser";
 import styles from "./EmailEditor.module.scss";
 import { Bold, Eraser, Italic, Underline } from "lucide-react";
+import { applyStyle } from "./apply-style";
 
 export default function EmailEditor() {
+  const [text, setText] = useState("lorem 512355555555");
+
+  const [selectionStart, setSelectionStart] = useState(0);
+  const [selectionEnd, setSelectionEnd] = useState(0);
+
+  const textRef = useRef(null);
+
+  const updateSelections = () => {
+    if (!textRef.current) return;
+
+    setSelectionStart(textRef.current.selectionStart);
+    setSelectionEnd(textRef.current.selectionEnd);
+  };
+
+  const applyFormat = (type) => {
+    const selectedText = text.substring(selectionStart, selectionEnd);
+    if (!selectedText) return;
+
+    const before = text.substring(0, selectionStart); // текст слева от выделенного фрагемента
+    const after = text.substring(selectionEnd); // текст справа от выделенного фрагемента
+
+    setText(before + applyStyle(type, selectedText) + after);
+  };
+
   return (
     <>
       <h1>Email editor</h1>
       <div className={styles.card}>
+        <span>Preview text</span>
+        <br />
+        {text && <div className={styles.preview}>{parse(text)}</div>}
+
         <textarea
+          ref={textRef}
           className={styles.editor}
-          placeholder="Write your message here"
-        >
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti,
-          consequatur!
-        </textarea>
+          spellCheck="false"
+          onClick={updateSelections}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <div className={styles.actions}>
           <div className={styles.tools}>
-            <button>
+            <button onClick={() => setText("")}>
               <Eraser />
             </button>
-            <button>
+            <button onClick={() => applyFormat("bold")}>
               <Bold />
             </button>
-            <button>
+            <button onClick={() => applyFormat("italic")}>
               <Italic />
             </button>
-            <button>
+            <button onClick={() => applyFormat("underline")}>
               <Underline />
             </button>
           </div>
---
+++ src/components/email-editor/apply-style.js
@@ -0,0 +1,21 @@
+export const applyStyle = (type, selectedText) => {
+  let formattedText = selectedText;
+
+  switch (type) {
+    case "bold":
+      formattedText = "<b>" + selectedText + "</b>";
+      break;
+    case "italic":
+      formattedText = "<i>" + selectedText + "</i>";
+      break;
+    case "underline":
+      formattedText = "<u>" + selectedText + "</u>";
+      break;
+
+    default:
+      formattedText = selectedText;
+      break;
+  }
+
+  return formattedText;
+};
